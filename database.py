8ecf3ed0a8a3d31606d19b3cfad53a253814f87d
Jorge-Jacinto <65885080+Jorge-Jacinto@users.noreply.github.com>
2024-10-18 11:36:20 -0600

    database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 import mysql.connector
 
 database = mysql.connector.connect(
-    host=os.getenv('DB_HOST', 'mysql.railway.internal'),
+    host=os.getenv('DB_HOST', 'autorack.proxy.rlwy.net'),
     user=os.getenv('DB_USER', 'root'),
     password=os.getenv('DB_PASS', 'wXNZqAGgLJvHEngOmHHWqRkYmGiNliqw'),
     database=os.getenv('DB_NAME', 'railway'),
