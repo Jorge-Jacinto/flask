4a8c22569302ae0c6da52a1aae6f57096502390a
Jorge-Jacinto <65885080+Jorge-Jacinto@users.noreply.github.com>
2024-10-22 23:26:09 -0600

    main.py

--- main.py
+++ main.py
@@ -487,4 +487,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=False, port=os.getenv("PORT", default=5000))
